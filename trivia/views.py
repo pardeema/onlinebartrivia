a6fc622af7db2c9c590c70cd5421b5ca2838906c
Marc Pardee <marc.pardee@utah.edu>
2020-06-20 22:01:43 -0600

    Added toggle to enable/disable gmae.
    Restructure for admin trivia paths

--- trivia/views.py
+++ trivia/views.py
@@ -8,13 +8,17 @@ def index(request):
     context = {'games': games}
     return render(request, 'index.html', context)
 
+def admin(request):
+    games = Game.objects.all()
+    context = {'games': games}
+    return render(request, 'admin.html', context)
 
-def game(request, game_id):
+def game_details(request, game_id):
     game = get_object_or_404(Game, password = game_id)
     rounds  = Round.objects.filter(game = game)
 
     context = {'game':game, 'rounds': rounds}
-    return render(request, 'game.html', context)
+    return render(request, 'game_details.html', context)
     
 def round(request, game_id, round_num):
     game = get_object_or_404(Game, password=game_id)
@@ -40,7 +44,7 @@ def add_round(request, game_id):
                         round = r )
             q.save()
 
-        return HttpResponseRedirect(reverse('game', args=(game_id,) ))
+        return HttpResponseRedirect(reverse('game_details', args=(game_id,) ))
     else:
         return render(request, 'add_round.html', {'game':game})
 
@@ -48,6 +52,15 @@ def add_game(request):
     if request.method == 'POST':
         g = Game(password=request.POST['g_id'], date=request.POST['date'])
         g.save()
-        return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse('admin'))
     else:
         return render(request, 'add_game.html')
+
+def toggle_game(request, game_id):
+    game = get_object_or_404(Game, password=game_id)
+    if game.active:
+        game.active = False
+    else:
+        game.active = True
+    game.save()
+    return HttpResponseRedirect(reverse('admin'))
