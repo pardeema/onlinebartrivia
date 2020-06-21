0713eabb737fcafd47f93b41a2219c4450871a04
Marc Pardee <marc.pardee@utah.edu>
2020-06-20 21:37:40 -0600

    Can upload a round w/ questions to a game.
    Added ability to add a new game.

--- trivia/urls.py
+++ trivia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+    path('add_game/', views.add_game, name='add_game'),
     path('game/<str:game_id>', views.game, name='game'),
     path('game/<str:game_id>/round/<int:round_num>', views.round, name='round'),
     path('game/<str:game_id>/add_round/', views.add_round, name='add_round')
